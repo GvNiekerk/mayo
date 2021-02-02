1e5b22fec736c31abf44e858f4698a2119991359
Ivan <ivan.snyman@gmail.com>
2021-02-02 14:02:43 +0200

    Worked on edit profile

--- src/editProfile.js
+++ src/editProfile.js
@@ -7,27 +7,72 @@ class ConfirmChanges extends Component {
     super(props);
 
     this.state = {
-      totalClicks: 0,
-      monthlyClicks: 0,
+     
       name: "N/A",
       location: "N/A",
-      active: "N/A",
-      contactInformation: {
+        basicdesc: "N/A",
+        detaildesc: "N/A",
         work: "N/A",
         website: "N/A",
         mail: "N/A",
         phone: "N/A",
         instaURL: "N/A",
         twitterURL: "N/A",
-        facebookURL: "N/A"
-
-      }
+        facebookURL: "N/A",
+        service: "",
+        hourlyRate: ""
+      
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.ConfirmChanges = this.ConfirmChanges.bind(this);
 
     this.getData();
   }
   //Add tags en location in user schema NB NB NB NB NB NB NB NB NB NB 
   //Add vanaand getelement by ID kak 
+  handleChange(event){
+    switch (event.target.name) {
+        case 'hourlyRate':
+            this.setState({ hourlyRate: event.target.value })
+            break;
+        case 'name':
+            this.setState({ name: event.target.value })
+            break;
+        case 'location':
+            this.setState({ location: event.target.value })
+            break;
+        case 'service':
+            this.setState({ work: event.target.value })
+            break;  
+        case 'website':
+            this.setState({ website: event.target.value })
+            break; 
+        case 'mail':
+            this.setState({ mail: event.target.value })
+            break; 
+        case 'phone':
+            this.setState({ phone: event.target.value })
+            break; 
+        case 'basicdesc':
+            this.setState({ basicdesc: event.target.value })
+            break;
+        case 'detaildesc':
+            this.setState({ detaildesc: event.target.value})
+            break;
+        case 'insta':
+            this.setState({ insta: event.target.value})
+            break;
+        case 'facebook': 
+            this.setState({ facebook: event.target.value})
+            break;
+        case 'twitter':
+            this.setState({ twitter: event.target.value})
+            break;
+    }
+}
+
+  
+
 
   getData = () => {
 
@@ -44,30 +89,70 @@ class ConfirmChanges extends Component {
       this.setState(responseJson);
     })
   }
-  /*
+
+  
+  
+
+
   ConfirmChanges(event){
     event.preventDefault();
     var headers = new Headers();
         headers.append('Content-Type', 'application/json')
     
     var req = {
-        userEmail: mail,
-        companyName: name,
-
+        userEmail: this.state.mail,
+        companyName: this.state.name,
+        basicDesc: this.state.basicdesc,
+        detailDesc: this.state.detaildesc,
+        cellNum: this.state.phone,
+        website: this.state.website,
+        hourlyRate: this.state.hourlyRate,
+        instaURL: this.state.insta,
+        twitterURL: this.state.twitter,
+        facebookURL: this.state.facebook,
+        image64: this.state.image64,
+        imageName: this.state.imageName,
+        adress: this.state.location
     }
     
-        fetch("http://localhost:3000/active/activate", {
+        fetch("http://localhost:3000/listing/update", {
             method: "post",
             headers: headers,
             body: JSON.stringify(req),
         }).then((response)=>{
             console.log(response.body);
-            if (response.ok){alert("User activated successfully");}
-            else{alert("Error activating user") }
+            if (response.ok){alert("User updated successfully");}
+            else{alert("Error updating user") }
         })
 
   }
-  */
+
+  handleUpload = (event) => {
+    event.preventDefault();
+
+    let file = event.target.files[0]
+    this.getBase64(file).then((base) => {
+        this.handleBase64(file, base);
+    });
+}
+
+handleBase64 = (file, base) => {
+    this.state.image64 = base
+    this.state.imageName = file.name
+    document.getElementById('test-image').setAttribute('src', base);
+}
+
+getBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+
+
+  
 
   render() {
     return (
@@ -89,7 +174,7 @@ class ConfirmChanges extends Component {
           monthly clicks
           <br/>
           <br/>
-          <div className="follow_count">{this.state.active.toString()}</div>
+          
           Active Account:
           <br/>
           <br/>
@@ -98,29 +183,36 @@ class ConfirmChanges extends Component {
           </div>
           <div className="right_col">
            
-            <input onChange={this.handleChange} name="hourlyRate" id="hourlyRate" className="follow_count" placeholder={this.state.hourlyRate} />
+            <input onChange={this.handleChange} name="hourlyRate" id="hourlyRate" className="follow_count" placeholder="Hourly Rate" />
             <br/>
-            <input onChange={this.handleChange} name="name" id="name" className="name" placeholder={this.state.name}/>
+            <input onChange={this.handleChange} name="name" id="name" className="name" placeholder="Company Name"/>
             <br/>
-            <input onChange={this.handleChange} name="location" id="location" className="location" placeholder={this.state.location} />
+            <input onChange={this.handleChange} name="location" id="location" className="location" placeholder="City" />
             <br/>
-            <input onChange={this.handleChange} name="work" id="work" className="work" placeholder={this.state.contactInformation.work} />
+            <select name="service" value={this.state.value} onChange={this.handleChange}>
+                        <option value="Plumbing">Plumbing</option>
+                        <option value="Electrical">Electrical</option>
+                        <option value="Gardening">Gardening</option>
+                        <option value="Construction">Construction</option>
+                    </select>            <br/>
+            <input onChange={this.handleChange} name="website" id="website" className="website" placeholder="Website URL" />
             <br/>
-            <input onChange={this.handleChange} name="website" id="website" className="website" placeholder={this.state.contactInformation.website} />
+            <input onChange={this.handleChange} name="mail" id="mail" className="mail" placeholder="Email Address" />
             <br/>
-            <input onChange={this.handleChange} name="mail" id="mail" className="mail" placeholder={this.state.contactInformation.mail} />
+            <input onChange={this.handleChange} name="phone" id="phone" className="phone" placeholder="Business Phone Number" />
             <br/>
-            <input onChange={this.handleChange} name="phone" id="phone" className="phone" placeholder={this.state.contactInformation.phone} />
+            <input onChange={this.handleChange} name="basicdesc" id="basicdesc" className="basic_desc" placeholder="Basic description of service" />
             <br/>
-            <input onChange={this.handleChange} name="basicdesc" id="basicdesc" className="basic_desc" placeholder={this.state.basicDesc} />
+            <input onChange={this.handleChange} name="detaildesc" id="detaildesc" className="detailed_desc" placeholder="Detailed description of service/services offered and prices" />
             <br/>
-            <input onChange={this.handleChange} name="detaildesc" id="detaildesc" className="detailed_desc" placeholder={this.state.detailDesc} />
+            <input onChange={this.handleChange} name="insta" id="insta" className="insta" placeholder="Company Instagram URL" />
             <br/>
-            <input onChange={this.handleChange} name="insta" id="insta" className="insta" placeholder={this.state.contactInformation.instaURL} />
+            <input onChange={this.handleChange} name="facebook" id="facebook" className="facebook" placeholder="Company Facebook URL" />
             <br/>
-            <input onChange={this.handleChange} name="facebook" id="facebook" className="facebook" placeholder={this.state.contactInformation.facebookURL} />
+            <input onChange={this.handleChange} name="twitter" id="twitter" className="twitter" placeholder="Company Twitter URL" />
             <br/>
-            <input onChange={this.handleChange} name="twitter" id="twitter" className="twitter" placeholder={this.state.contactInformation.twitterURL} />
+            <input onChange={this.handleUpload} type="file" className="fileup" name="file" />
+                    <img id="test-image" style={{width: 200, height: 200 }} />
             <br/>
             
 
